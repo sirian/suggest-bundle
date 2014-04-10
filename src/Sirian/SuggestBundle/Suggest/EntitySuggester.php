caaddf5c1a3a17317414aee46fec277453b59dc1
Vasily Khayrulin <sirianru@gmail.com>
2014-04-10 12:34:33 +0400

    Added EntitySuggester

--- src/Sirian/SuggestBundle/Suggest/EntitySuggester.php
+++ src/Sirian/SuggestBundle/Suggest/EntitySuggester.php
@@ -2,12 +2,55 @@
 
 namespace Sirian\SuggestBundle\Suggest;
 
+use Doctrine\ORM\QueryBuilder;
 use Symfony\Bridge\Doctrine\Form\ChoiceList\ORMQueryBuilderLoader;
 use Symfony\Component\OptionsResolver\Options;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class EntitySuggester extends DoctrineSuggester
 {
-    //todo
+    protected function getLoader()
+    {
+        $options = $this->options;
+        return new ORMQueryBuilderLoader($this->createQueryBuilder(), $options['manager'], $options['class']);
+    }
 
+    protected function getSuggestLoader(SuggestQuery $query)
+    {
+        $options = $this->options;
+        return new ORMQueryBuilderLoader($this->createSuggestQueryBuilder($query), $options['manager'], $options['class']);
+    }
+
+    /**
+     * @return QueryBuilder
+     */
+    protected function createQueryBuilder()
+    {
+        return $this->getManager()->getRepository($this->options['class'])->createQueryBuilder('e');
+    }
+
+    protected function createSuggestQueryBuilder(SuggestQuery $query)
+    {
+        $qb = $this->createQueryBuilder();
+        $alias = $qb->getRootAlias();
+        $offset = max(0, $this->options['limit'] * ($query->page - 1));
+
+
+        $or = $qb->expr()->orX();
+        foreach ($this->options['search'] as $field => $searchType) {
+
+            $suffix = in_array($searchType, ['prefix', 'middle']) ? '%' : '';
+            $prefix = in_array($searchType, ['suffix', 'middle']) ? '%' : '';
+            $or->add($alias . '.' . $field . ' LIKE :suggest_' . $field);
+            $qb->setParameter('suggest_' . $field, $prefix . $query->searchTerm . $suffix);
+        }
+
+        $qb
+            ->andWhere($or)
+            ->setFirstResult($offset)
+            ->setMaxResults($this->options['limit'] + 1)
+        ;
+
+        return $qb;
+    }
 }
